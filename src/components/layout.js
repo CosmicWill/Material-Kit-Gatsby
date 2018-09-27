e509e1c444190479380f5df925fccaa257c61c50
CosmicWill <wilfredor@mail.usf.edu>
2018-09-26 21:21:18 -0500

    init commit

---
+++ src/components/layout.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Header from './Header/Header'
+import './layout.css'
+
+import "assets/slick/slick.css";
+//import "assets/slick/slick-theme.css";
+import "assets/scss/material-kit-react.css?v=1.2.0";
+
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        > 
+          <html lang="en" />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"/>
+
+
+        </Helmet>
+        <Header brand={data.site.siteMetadata.title} />
+        <div>
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link } from 'gatsby'
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+import SectionBasics from "../Sections/SectionBasics.jsx";
+import SectionNavbars from "../Sections/SectionNavbars.jsx";
+import SectionTabs from "../Sections/SectionTabs.jsx";
+import SectionPills from "../Sections/SectionPills.jsx";
+import SectionNotifications from "../Sections/SectionNotifications.jsx";
+import SectionTypography from "../Sections/SectionTypography.jsx";
+import SectionCarousel from "../Sections/SectionCarousel.jsx";
+import SectionCompletedExamples from "../Sections/SectionCompletedExamples.jsx";
+import SectionLogin from "../Sections/SectionLogin.jsx";
+import SectionJavascript from "../Sections/SectionJavascript.jsx";
+import Parallax from "components/Parallax/Parallax.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Layout from '../components/layout';
+import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
+
+
+class Components extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Layout>
+        <Parallax image={require("assets/img/bg4.jpg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem>
+                <div className={classes.brand}>
+                  <h1 className={classes.title}>Material Kit React.</h1>
+                  <h3 className={classes.subtitle}>
+                    A Badass Material-UI Kit based on Material Design.
+                  </h3>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <SectionBasics />
+          <SectionNavbars />
+          <SectionTabs />
+          <SectionPills />
+          <SectionNotifications />
+          <SectionTypography />
+          <SectionJavascript />
+          <SectionCarousel />
+          <SectionCompletedExamples />
+          <SectionLogin />
+        </div>
+        
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
+}
+
+
+export default withStyles(componentsStyle)(Components);
